bd8f6f7e875bf64b4042ddbce0b6d87b1e468ffe
Godfrey <godfreyanthony3245.eng@gmail.com>
2026-07-31 00:17:48 +0100

    Create Config.js
    this the configuration JavaScript code

---
+++ Config.js
@@ -0,0 +1,28 @@
+// Application Configuration Keys, System Base Constants & Location Data
+window.APP_CONFIG = {
+    API_KEYS: {
+        EMAILJS_PUBLIC_KEY: "u-NOux4hS6c0LcdrY",
+        EMAILJS_SERVICE_ID: "service_d6d90uf",
+        EMAILJS_TEMPLATE_ID: "template_5m1rlup",
+        SDK_URL: "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js",
+        TIMEOUT_MS: 15000
+    },
+    FIREBASE: {
+        apiKey: "AIzaSyBXpwBybezJScaQz05svZX3vaTd2ds5yvU",
+        authDomain: "g-tech-anthofix.firebaseapp.com",
+        projectId: "g-tech-anthofix",
+        storageBucket: "g-tech-anthofix.firebasestorage.app",
+        messagingSenderId: "830606678445",
+        appId: "1:830606678445:web:c172d676b5701729fde924",
+        measurementId: "G-KXMJZRE3VZ"
+    }
+};
+
+window.LocationData = {
+    "Nigeria": {
+        "Delta State": ["Oshimili South (Asaba)", "Oshimili North", "Aniocha South", "Aniocha North", "Ethiope East", "Ethiope West", "Okpe", "Sapele", "Uvwie", "Warri South"],
+        "Lagos State": ["Ikeja", "Lagos Island", "Surulere", "Alimosho", "Ikorodu"]
+    },
+    "Ghana": { "Greater Accra": ["Accra Metropolitan", "Tema"] },
+    "Other": { "International Location": ["General Municipal Area"] }
+};
